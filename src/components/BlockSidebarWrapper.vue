0bfd7f1ce713dee2cc451bcf7d03b11ff610a9de
1wairesd <149899427+1wairesd@users.noreply.github.com>
2025-07-15 23:42:29 +0000

    replace

--- src/components/BlockSidebarWrapper.vue
+++ src/components/BlockSidebarWrapper.vue
@@ -1,28 +1,39 @@
 <script setup>
-import { defineProps, defineEmits } from 'vue';
-import { ReactWrapper } from 'vue3-react-wrapper';
-import BlockSidebar from './BlockSidebar.react.jsx';
+import { ref, onMounted, onBeforeUnmount, watch } from 'vue'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BlockSidebar from './BlockSidebar.react.jsx'
 
 const props = defineProps({
   node: Object,
   isRoot: Boolean,
-});
-const emit = defineEmits(['update:node', 'delete']);
+})
+const emit = defineEmits(['update:node', 'delete'])
 
-const handleChange = (updatedNode) => {
-  emit('update:node', updatedNode);
-};
-const handleDelete = () => {
-  emit('delete');
-};
+const container = ref(null)
+let root = null
+
+const renderReact = () => {
+  if (container.value) {
+    if (!root) root = ReactDOM.createRoot(container.value)
+    root.render(
+      React.createElement(BlockSidebar, {
+        node: props.node,
+        isRoot: props.isRoot,
+        onChange: (n) => emit('update:node', n),
+        onDelete: () => emit('delete'),
+      })
+    )
+  }
+}
+
+onMounted(renderReact)
+watch(() => [props.node, props.isRoot], renderReact)
+onBeforeUnmount(() => {
+  if (root) root.unmount()
+})
 </script>
 
 <template>
-  <ReactWrapper
-    :component="BlockSidebar"
-    :node="node"
-    :isRoot="isRoot"
-    :onChange="handleChange"
-    :onDelete="handleDelete"
-  />
+  <div ref="container"></div>
 </template> 
